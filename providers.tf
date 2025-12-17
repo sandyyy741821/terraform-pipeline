7cd6347a33b16d54b703789982e197aaa47e8766
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-12-17 19:43:23 +0530

    Initial Terraform project with secure backend and CI/CD pipeline

--- providers.tf
+++ providers.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = "~>1.6.0"
   required_providers {
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~>4.1.0"
-    }
+    # tls = {
+    #   source  = "hashicorp/tls"
+    #   version = "~>4.1.0"
+    # }
     aws = {
       source  = "hashicorp/aws"
       version = "~>6.9.0"
@@ -14,8 +14,8 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  # assume_role {
-  #   role_arn = var.assume_role
-  # }
+  assume_role {
+    role_arn = var.assume_role
+  }
 }
 
